e28f7f2c345bf229e310aa546844120091ec3761
Pranav Khade <pranav.m.khade@gmail.com>
2021-06-02 15:26:54 -0500

    VCF reader improvements

--- python_scripts/VCFanalyse.py
+++ python_scripts/VCFanalyse.py
@@ -7,19 +7,30 @@ def read_vcf(fh):
 
 	for i in fh:
 		if(i[0:2]=='##'):
-			metadata.append(i)
+			metadata.append(i.strip())
 		elif(i[0:6]=='#CHROM'):
-			DF = DataFrame( columns=i.split() )
+			DF = DataFrame( columns=i.strip().split() )
 		else:
 			#DF.append( { DF.columns[numj]:j for numj,j in enumerate(i.split()) } , ignore_index=True)
-			DF = DF.append( DataFrame( [[j for j in i.split()]] , columns= list(DF.columns) ) )
-	
+			try:
+				DF = DF.append( DataFrame( [[j for j in i.strip().split()]] , columns= list(DF.columns) ) )
+			except:
+				new_columns = [ 'col'+str(j+1) for j in range(len(DF.columns), len(DF.columns)+len(i.split())-len(DF.columns) ) ]
+				for j in new_columns: DF[j] = None
+				DF = DF.append( DataFrame( [[j for j in i.strip().split()]] , columns= list(DF.columns) ) )
+
 	return DF, metadata
 
 
 def main():
-	fh = open('NA19461.final.manta.svtyped.vcf')
-	data = read_vcf(fh)
+	#example
+	#fh = open('NA19461.final.manta.svtyped.vcf')
+	fh = open('NA19461.final.breakdancer.vcf')
+	records , metadata = read_vcf(fh)
+
+	print(records)
+
+	
 
 
 if(__name__=='__main__'):
